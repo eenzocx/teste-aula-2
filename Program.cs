84d928cc5b773016bc63226c91569b4059418cff
Enzo Miguel <enzo29620@gmail.com>
2023-08-11 22:05:06 -0300

    sla

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Beep(3000, 1136);
+Console.Beep();
+Console.Beep();
+Thread.Sleep (3000);
+Console.Beep();
+Console.Beep();
+
+Console.BackgroundColor =  ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("ola mundo");
+Thread.Sleep (3000);
+Console.WriteLine("eu estou vivo");
+Thread.Sleep (3000);
+Console.WriteLine("OH MY GOD");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("EU ESTOU VIVO HALLELUJAH DIO");
+Console.ResetColor();
+
